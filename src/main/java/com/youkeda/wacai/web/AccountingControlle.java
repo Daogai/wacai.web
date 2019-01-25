f33ffddf6f40c13aa91b295430978f9cd1b06893
Daogai <46884974+Daogai@users.noreply.github.com>
2019-01-25 18:07:24 +0800

    提交作业
    提交作业

--- src/main/java/com/youkeda/wacai/web/AccountingControlle.java
+++ src/main/java/com/youkeda/wacai/web/AccountingControlle.java
@@ -1,30 +1,26 @@
 package com.youkeda.wacai.web;
 
+import com.youkeda.wacai.web.model.Accounting;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountingControlle {
-@RequestMapping(path ="/account")
-public String account(@RequestParam("cash")int cash,@RequestParam("rent")int rent,@RequestParam("income")int income,@RequestParam("charges")int charges,@RequestParam("eat")int eat)
-{
-    int result =cash+income-rent-charges-eat;
-    String a ="本月余额：";
-    return a+result;
+@RequestMapping(path ="/accounting")
+public Accounting accounting(Accounting accounting){
+    int result=accounting.getcash() +
+               accounting.getincome()-
+               accounting.getrent()-
+               accounting.getbalance()-
+               accounting.geteat()-
+               accounting.getktv()-
+               accounting.getbalance();
+               accounting.setbalance(result);
 
-
-
-
-
-
-
-
-
-
-
+               return accounting;
 
 }
+
 
 
 
---
+++ src/main/java/com/youkeda/wacai/web/model/Accounting.java
@@ -0,0 +1,79 @@
+package com.youkeda.wacai.web.model;
+
+public class Accounting {
+
+      private int cash;
+      private int income;
+      private int rent;
+      private int charges;
+      private int eat;
+      private int treat;
+      private int ktv;
+      private int balance;
+      public int getcash() {
+          return cash;
+    }
+
+    public void setcash(int cash) {
+        this.cash = cash;
+    }
+
+    public int getincome() {
+        return income;
+    }
+
+    public void setincome(int income) {
+        this.income = income;
+    }
+
+    public int getrent() {
+        return rent;
+    }
+
+    public void setrent(int rent) {
+        this.rent = rent;
+    }
+
+    public int getcharges() {
+        return charges;
+    }
+
+    public void setcharges(int charges) {
+        this.charges = charges;
+    }
+
+    public int geteat() {
+        return eat;
+    }
+
+    public void seteat(int eat) {
+        this.eat = eat;
+    }
+
+    public int gettreat() {
+        return treat;
+    }
+
+    public void settreat(int treat) {
+        this.treat = treat;
+    }
+
+    public int getktv() {
+        return ktv;
+    }
+
+    public void setktv(int ktv) {
+        this.ktv = ktv;
+    }
+
+    public int getbalance() {
+        return balance;
+    }
+
+    public void setbalance(int balance) {
+        this.balance = balance;
+    }
+
+  }
+
+
